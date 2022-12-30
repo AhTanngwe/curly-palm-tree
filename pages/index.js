64fb5bf02351e947c32130f9dd2982eaa9b94478
AhTanngwe <100001404+AhTanngwe@users.noreply.github.com>
2022-12-30 16:52:14 +0000

    Pending changes exported from your codespace

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-importimportimportimportimportimportimportimport { useCallback, useEffect, useState } from 'react'
++importimportimportimportimportimportimportimport { useCallback, useEffect, useState } from 'react'
 import Button from '../components/Button'
 import ClickCount from '../components/ClickCount'
 import styles from '../styles/home.module.css'
